6682744cedea659844892ef9502ce067e22729ec
José Argentino da Silva Felipe <j.argentino@hotmail.com>
2025-10-05 12:45:12 -0300

    interno

--- index.php
+++ index.php
@@ -30,23 +30,9 @@
 <body>
     <div id="container">
         <header>
-            <div id="logoPequena">
-            <img class="logoPequenaImg" src="loja/img/logo/logo_1.jpg" alt="">
-            </div>
-
-            <div id="nome">
-                <h1>Nome da Loja</h1>
-                <h4>P ao Plus Size</h4>
-            </div>
-
-            <div id="navBarExterno">
-                <a href="nova_senha.php">
-                    <h4> 
-                        <span>Redefinir</span>
-                        <span>Senha</span>
-                    </h4>
-                </a>
-            </div>
+           <?php 
+                include "loja/php/partes/menuExterno.php"
+           ?>
         </header>
 
         <main>
---
+++ loja/php/partes/menuInterno.php
@@ -0,0 +1,19 @@
+<div id="logoPequena">
+    <img class="logoPequenaImg" src="img/logo/logo_1.jpg" alt="">
+</div>
+
+<div id="nome">
+    <h1>Nome da Loja</h1>
+    <h4>P ao Plus Size</h4>
+</div>
+
+<div id="navBar">
+    <i class="fas fa-bars menu-toggle"></i> <!-- Ícone hambúrguer -->
+    <nav id="menu">
+        <ul>
+            <li><a href="#">Vendas</a></li>
+            <li><a href="#">Entradas</a></li>
+            <li><a href="#">Estoque</a></li>
+            <li><a href="cadastro_use.php">Cadastro Usuário</a></li>
+        </ul>
+    </nav>
