0a86d7171c2212d5ac395c22b823eb9e066028af
José Argentino da Silva Felipe <j.argentino@hotmail.com>
2025-10-05 12:16:19 -0300

    menu interno ok

--- loja/home.php
+++ loja/home.php
@@ -29,12 +29,12 @@
         <div id="navBar">
             <i class="fas fa-bars menu-toggle"></i> <!-- Ícone hambúrguer -->
             <nav id="menu">
-            <ul>
-                <li><a href="#">Vendas</a></li>
-                <li><a href="#">Entradas</a></li>
-                <li><a href="#">Estoque</a></li>
-                <li><a href="cadastro_use.php">Cadastro Usuário</a></li>
-            </ul>
+                <ul>
+                    <li><a href="#">Vendas</a></li>
+                    <li><a href="#">Entradas</a></li>
+                    <li><a href="#">Estoque</a></li>
+                    <li><a href="cadastro_use.php">Cadastro Usuário</a></li>
+                </ul>
             </nav>
         </div>
         
