fddbfa32a55efa7ae25a36bcec27631114e9c1c1
José Argentino da Silva Felipe <j.argentino@hotmail.com>
2025-11-14 11:22:08 -0300

    erro geral

--- loja/php/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();       // Garante que a sessão esteja ativa
-session_unset();       // Remove todas as variáveis da sessão
-session_destroy();     // Encerra a sessão
-
-// Redireciona de volta para o login
-header("Location: ../../index.php?erro=" . urlencode("Sessão encerrada com sucesso"));
-exit;
-?>
